e869b7524d23d784d09ff0cd4c74e209eefeb0f7
Sana Ajani <sanaajani@msn.com>
2019-02-06 17:01:55 -0800

    Change gettodos() response handling

--- app/routes.js
+++ app/routes.js
@@ -1,24 +1,18 @@
 var Todo = require('./models/todo');
 
-function getTodos(res) {
-    Todo.find(function (err, todos) {
-
-        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(todos); // return all todos in JSON format
-    });
+function getTodos() {
+    return Todo.find().exec();
 };
+
 
 module.exports = function (app) {
 
     // api ---------------------------------------------------------------------
     // get all todos
-    app.get('/api/todos', function (req, res) {
+    app.get('/api/todos', async function (req, res) {
         // use mongoose to get all todos in the database
-        getTodos(res);
+        let todos = await getTodos();
+        res.json(todos);
     });
 
     // create todo and send back all todos after creation
@@ -52,6 +46,6 @@ module.exports = function (app) {
 
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(__dirname + '/../public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 };
